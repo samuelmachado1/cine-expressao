4206b410d9b6675907cb0030fc4640e2770ac197
Samuel Machado <samuelmachado1@gmail.com>
2022-04-06 00:15:34 -0300

    [CreateRoutes] First settings and routes.

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,4 +5,27 @@ const usersRoute = require('./routes/users');
 
 app.use('/users', usersRoute);
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  return res.send({
+    error: {
+      message: error.message
+    }
+  });
+
+  res.status({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
 module.exports = app;
--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -13,4 +13,29 @@ router.post('/', (req, res, next) => {
   });
 });
 
+router.get('/:userId', (req, res, next) => {
+  const id = req.params.userId;
+  res.status(200).send({
+    message: 'GET with user!!! It works!',
+    id: id
+  });
+});
+
+router.patch('/:userId', (req, res, next) => {
+  const id = req.params.userId;
+  res.status(200).send({
+    message: 'PATCH!!! It works!',
+    id: id
+  });
+});
+
+router.delete('/', (req, res, next) => {
+  const id = req.params.userId;
+  res.status(200).send({
+    message: 'DELETE!!! It works!',
+    id: id
+  });
+});
+
+
 module.exports = router;
